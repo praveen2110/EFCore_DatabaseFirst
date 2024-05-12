1c8e8631a731040800858d4124bbe09ce08e2b7c
Praveen <praveen211096@gmail.com>
2024-05-12 12:38:24 +0530

    Add project files.

---
+++ EFCore_DatabaseFirst/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_DatabaseFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {        
+            using (EFDemoEntity context = new EFDemoEntity())
+            {
+                Student student = new Student();
+
+                /* student.FirstName = "Test";
+                 student.LastName = "Test";
+                 student.StandardId = 1;
+
+                 context.Students.Add(student);*/
+
+                /*var Allstudent = context.Students.ToList();
+
+                foreach (var stud in Allstudent)
+                {
+                    Console.WriteLine(stud.LastName);
+                }
+                Console.ReadKey();*/
+
+                /*student = context.Students.Find(19);
+
+                student.FirstName = "TestUpdate";*/
+
+                /*student = context.Students.Find(19);
+                context.Students.Remove(student);*/
+
+                //context.SaveChanges();
+
+
+            }
+        }
+    }
+}
